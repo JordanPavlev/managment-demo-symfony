db9e453e0540662d3d4cf2307f08346e786854ea
JordanPavlev <danipavlev@gmail.com>
2023-06-26 20:06:06 +0300

    added products page

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    public function __construct(private readonly EntityManagerInterface $em){}
+
+    #[Route('/shop', name: 'app_product')]
+    public function index(): Response
+    {
+        $products = $this->em->getRepository(Product::class)->findAll();
+        return $this->render('product/index.html.twig', [
+            // 'controller_name' => 'ProductController',
+            "products" => $products,
+        ]);
+    }
+}
